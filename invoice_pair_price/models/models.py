862b423e0c065c9fa9e22d714e6f53128a996ea5
Ismail Khan <73880897+ismailwaxer@users.noreply.github.com>
2022-06-07 19:34:47 +0500

    updated

--- invoice_pair_price/models/models.py
+++ invoice_pair_price/models/models.py
@@ -1,25 +1,25 @@
-# # -*- coding: utf-8 -*-
-#
-# from odoo import models, fields, api
-#
-# class InheritInvoiceLIne(models.Model):
-# 	_inherit = 'account.move.line'
-#
-# 	x_studio_pair_price = fields.Float('Pair Price')
-#
-#
-#
-# class InheritSaleOrderline(models.Model):
-# 	_inherit = 'sale.order.line'
-#
-# 	def _prepare_invoice_line(self, **optional_values):
-# 		# Updating discount amount (in float) on invoice line
-# 		res = super(InheritInvoiceLIne, self)._prepare_invoice_line(**optional_values)
-# 		for rec in self:
-# 			if rec.x_studio_pair_price:
-# 				res.update({'x_studio_pair_price': rec.x_studio_pair_price})
-# 			else:
-# 				continue
-# 		return res
-#
-#
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class InheritInvoiceLIne(models.Model):
+	_inherit = 'account.move.line'
+
+	x_studio_pair_price = fields.Float('Pair Price')
+
+
+
+class InheritSaleOrderline(models.Model):
+	_inherit = 'sale.order.line'
+
+	# def _prepare_invoice_line(self, **optional_values):
+	# 	# Updating discount amount (in float) on invoice line
+	# 	res = super(InheritInvoiceLIne, self)._prepare_invoice_line(**optional_values)
+	# 	for rec in self:
+	# 		if rec.x_studio_pair_price:
+	# 			res.update({'x_studio_pair_price': rec.x_studio_pair_price})
+	# 		else:
+	# 			continue
+	# 	return res
+
+
